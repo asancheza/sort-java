fbb5866c632a840f004dc8889b8299fbd80f9133
Alejandro Sanchez Acosta <asanchez@neurowork.net>
2016-02-21 15:31:25 -0800

    Add comments

--- Sorts.java
+++ Sorts.java
@@ -1,9 +1,21 @@
+/*
+ * Stack class
+ * 
+ * @author Alejandro Sanchez Acosta
+ */
+
 import java.util.Random;
 
 public class Sorts {
+  /* 
+   * Selection Sort
+   */
   public static <T extends Comparable<T>> int numberOfComparisonInSelectionSort(T[] arr) {
   	int compares = 0;
 
+    /* 
+     * Compares next into the current index to find the minimal and swap
+     */
     for (int i = 0; i < arr.length - 1; i++) {
       int minIndex = i;
 
@@ -22,10 +34,16 @@ public class Sorts {
     return compares;
   }
 
+  /* 
+   * Shell sort
+   */
   public static <T extends Comparable<? super T>> int numberOfComparisonInShellSort(T[] a) {
     int compares = 0;
     int j;
 
+    /* 
+     * Compare intervals (middle and later one less and select minimal)
+     */
     for (int gap = a.length / 2; gap > 0; gap /= 2) {
       for (int i = gap; i < a.length; i++) {
          T tmp = a[i];
@@ -40,6 +58,9 @@ public class Sorts {
     return compares;
   }
 
+  /* 
+   * Generate random numbers
+   */
   public static Integer[] generateRandomArrayWithLength(int length) {
     Random randomGenerator = new Random();
     Integer[] arr = new Integer[length];
@@ -51,6 +72,9 @@ public class Sorts {
     return arr;
   }
 
+  /* 
+   * Try both implementations with different arrays sizes
+   */
   public static void main(String[] args) {
     Integer[] data = new Integer[5];
     data[0] = 5;
@@ -58,7 +82,7 @@ public class Sorts {
     data[2] = 3;
     data[3] = 2;
     data[4] = 1;
-    System.out.println("Selection Sort - Number compares simple array(5): " + numberOfComparisonInSelectionSort(data));
+    System.out.println("Selection Sort - Number of comparisons with simple array(5): " + numberOfComparisonInSelectionSort(data));
 
     Integer[] data2 = new Integer[5];
     data2[0] = 20;
@@ -66,18 +90,18 @@ public class Sorts {
     data2[2] = 3;
     data2[3] = 2;
     data2[4] = 1;
-    System.out.println("Shell Sort - Number compares simple array(5): " + numberOfComparisonInShellSort(data2));
+    System.out.println("Shell Sort - Number of comparisons with simple array(5): " + numberOfComparisonInShellSort(data2));
 
     Integer[] ar1 = generateRandomArrayWithLength(100000);
-    System.out.println("Selection Sort - Number compares array 100000: " + numberOfComparisonInSelectionSort(ar1));
+    System.out.println("Selection Sort - Number of comparisons with array 100000: " + numberOfComparisonInSelectionSort(ar1));
 
     Integer[] ar2 = generateRandomArrayWithLength(100000);
-    System.out.println("Shell Sort - Number compares array 100000: " + numberOfComparisonInShellSort(ar2));
+    System.out.println("Shell Sort - Number of comparisons with array 100000: " + numberOfComparisonInShellSort(ar2));
 
-    Integer[] ar3 = generateRandomArrayWithLength(10000000);
-    System.out.println("Selection Sort - Number compares array 10000000: " + numberOfComparisonInSelectionSort(ar3));
+    Integer[] ar3 = generateRandomArrayWithLength(1000000);
+    System.out.println("Selection Sort - Number of comparisons with array 10000000: " + numberOfComparisonInSelectionSort(ar3));
 
-    Integer[] ar4 = generateRandomArrayWithLength(10000000);
-    System.out.println("Shell Sort - Number compares array 10000000: " + numberOfComparisonInShellSort(ar4));
+    Integer[] ar4 = generateRandomArrayWithLength(1000000);
+    System.out.println("Shell Sort - Number of comparisons with array 10000000: " + numberOfComparisonInShellSort(ar4));
   }
 }
